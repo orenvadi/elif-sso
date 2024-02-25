a37ccc160a8e0015202dae4abc98283704d1c538
Narashi <orenvady@gmail.com>
2024-02-26 01:55:05 +0600

    added Dockerfile, changed config from .yaml to .env

--- go.mod
+++ go.mod
@@ -8,9 +8,9 @@ require (
 	github.com/fatih/color v1.16.0
 	github.com/golang-jwt/jwt/v5 v5.2.0
 	github.com/golang-migrate/migrate/v4 v4.17.0
-	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jackc/pgx/v4 v4.18.1
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22
 	golang.org/x/crypto v0.17.0
 	google.golang.org/grpc v1.61.1
@@ -18,7 +18,6 @@ require (
 )
 
 require (
-	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/cel-go v0.19.0 // indirect
@@ -31,7 +30,6 @@ require (
 	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -43,6 +41,4 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,26 @@
 package config
 
 import (
-	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Env      string `yaml:"env" env-default:"local"` // it's better not to use in prod
-	Storage  `yaml:"storage"`
-	TokenTTL time.Duration `yaml:"token_ttl" env-required:"true"`
-	GRPC     `yaml:"grpc"`
+	Env      string // No need for env-default tag, we'll set defaults directly in the code
+	Storage  Storage
+	TokenTTL time.Duration
+	GRPC     GRPC
 }
 
 type Storage struct {
-	User     string `yaml:"user" env-default:"postgres"`
-	Password string `yaml:"password" env-default:"postgres"`
-	Host     string `yaml:"host" env-default:"localhost"`
-	DbName   string `yaml:"db_name" env-required:"true"`
+	User     string
+	Password string
+	Host     string
+	DbName   string
 }
 
 func (s Storage) DSN() string {
@@ -28,40 +28,71 @@ func (s Storage) DSN() string {
 }
 
 type GRPC struct {
-	Port    int           `yaml:"port"`
-	Timeout time.Duration `yaml:"timeout"`
+	Port    int
+	Timeout time.Duration
 }
 
-func MustLoad() *Config { // this func will not return error, but panic instead
-	path := fetchConfigPath()
-	if path == "" {
-		panic("config path is empty")
-	}
-
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		panic("config file does not exist" + path)
+func MustLoad() *Config {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic(fmt.Sprintf("Error loading .env file: %v", err))
 	}
 
 	var cfg Config
 
-	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
-		panic("failed to read config" + err.Error())
+	// Retrieve environment variables and set defaults
+	cfg.Env = os.Getenv("ENV")
+	if cfg.Env == "" {
+		cfg.Env = "local" // Default value if ENV is not set
 	}
+
+	cfg.Storage.User = os.Getenv("STORAGE_USER")
+	if cfg.Storage.User == "" {
+		cfg.Storage.User = "postgres" // Default value if STORAGE_USER is not set
+	}
+	cfg.Storage.Password = os.Getenv("STORAGE_PASSWORD")
+	if cfg.Storage.Password == "" {
+		cfg.Storage.Password = "postgres" // Default value if STORAGE_PASSWORD is not set
+	}
+	cfg.Storage.Host = os.Getenv("STORAGE_HOST")
+	if cfg.Storage.Host == "" {
+		cfg.Storage.Host = "localhost" // Default value if STORAGE_HOST is not set
+	}
+	cfg.Storage.DbName = os.Getenv("STORAGE_DB_NAME")
+	if cfg.Storage.DbName == "" {
+		panic("STORAGE_DB_NAME environment variable is required")
+	}
+
+	tokenTTLStr := os.Getenv("TOKEN_TTL")
+	if tokenTTLStr == "" {
+		panic("TOKEN_TTL environment variable is required")
+	}
+	tokenTTL, err := time.ParseDuration(tokenTTLStr)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse TOKEN_TTL: %s", err))
+	}
+	cfg.TokenTTL = tokenTTL
+
+	grpcPortStr := os.Getenv("GRPC_PORT")
+	if grpcPortStr == "" {
+		panic("GRPC_PORT environment variable is required")
+	}
+	grpcPort, err := strconv.Atoi(grpcPortStr)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse GRPC_PORT: %s", err))
+	}
+	cfg.GRPC.Port = grpcPort
+
+	grpcTimeoutStr := os.Getenv("GRPC_TIMEOUT")
+	if grpcTimeoutStr == "" {
+		panic("GRPC_TIMEOUT environment variable is required")
+	}
+	grpcTimeout, err := time.ParseDuration(grpcTimeoutStr)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse GRPC_TIMEOUT: %s", err))
+	}
+	cfg.GRPC.Timeout = grpcTimeout
 
 	return &cfg
-}
-
-// fetchConfigPath fetches config path from command line flag or environment variable.
-// Priority: flag > env > default.
-// Default value is empty string.
-func fetchConfigPath() string {
-	var res string
-
-	flag.StringVar(&res, "config", "", "path to config value")
-	flag.Parse()
-
-	if res == "" {
-		res = os.Getenv("CONFIG_PATH")
-	}
-	return res
 }
